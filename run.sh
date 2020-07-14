b8d358105bf2f0d7af4f539c5bcdc9b723facc56
alex <alex2528@live.fr>
2020-07-14 02:03:58 +0200

    de

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!bin/sh
+ssh ubuntu@15.236.91.165 <<EOF
+cd ~/rgpd_front
+sudo -s
+git pull origin master
+ npm install --save --unsafe-perm
+
+npm audit fix --force
+npm run build
+rm -rf ../backrgpd/dist/*
+mv  build/* ../backrgpd/dist/
+exit
+EOF
