e98881fb5a2d769fb36f2d25914b299b2709ce1e
alex <alex2528@live.fr>
2020-07-14 12:02:59 +0200

    c

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!bin/sh
-ssh ubuntu@15.236.91.165 <<EOF
+sudo -s
+ssh  ubuntu@15.236.91.165 <<EOF
 cd ~/rgpd_front
 sudo -s
 git pull origin master
-nvm use 12.18.0
+
 npm install -g @angular/cli
  npm install --save --unsafe-perm
 
